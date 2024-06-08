002ab1f755ea3f2afbb9bec9870cf3ec6945e08e
Vishnvardhan <71860406+vishnuvardhan015@users.noreply.github.com>
2024-06-08 11:07:37 +0530

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,11 @@
+const Main = () => {
+  return (
+    <section>
+      <div className="container">
+        <div className="square"></div>
+      </div>
+    </section>
+  );
+};
+
+export default Main;
